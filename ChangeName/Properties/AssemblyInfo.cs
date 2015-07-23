b7882b291a52d35528e590f0a48ddc42b253be13
Zaicon Kiroshu <kzaicon@yahoo.com>
2015-07-23 12:08:39 -0500

    Initial Commit

---
+++ ChangeName/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ChangeName")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChangeName")]
+[assembly: AssemblyCopyright("Copyright © Simon311 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
